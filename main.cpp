d750689242e5d1d560244b7af6394d8baa41ec60
Lauren <lat.mail.lat@gmail.com>
2025-01-14 15:54:27 +0000

    started project added test code

--- main.cpp
+++ main.cpp
@@ -1,20 +1,9 @@
-// Your First C++ Program
-
 using namespace std;
 #include <iostream>
 #include <string>
 
-class Food {
-    public:
-        float price;
-        string name;
-};
 
 int main() {
-    Food apple;
-    apple.name = "pink lady";
-    apple.price = 1.30;
-    
-    cout <<"Item 01 = "<<apple.name<<" £"<<apple.price<< endl;
+   cout<<"Hello world";
     return 0;
 }
